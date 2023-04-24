26fe7f8974697278493502bafff7cada4f49ede9
LucasFlaquer <lucas.flaquer@gmail.com>
2023-04-24 19:19:52 -0300

    repository interface

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,19 @@
+import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
+import { Prisma, User } from '@prisma/client'
+
+export class PrismaUserRepository implements UsersRepository {
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({ data })
+    return user
+  }
+}
