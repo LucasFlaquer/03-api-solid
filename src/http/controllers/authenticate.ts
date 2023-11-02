52a1d5b2f19dc20ebbde618950c05aba7f99b751
LucasFlaquer <lucas.flaquer@gmail.com>
2023-11-02 10:48:46 -0300

    update controllers

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
-import { makeAuthenticateUseCase } from '@/use-cases/make-authenticate-use-case'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(
   request: FastifyRequest,
@@ -14,7 +14,9 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body)
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+    const token = await reply.jwtSign({}, { sign: { sub: user.id } })
+    return reply.status(200).send({ token })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({
@@ -23,5 +25,4 @@ export async function authenticate(
     }
     throw error
   }
-  return reply.status(200).send()
 }
