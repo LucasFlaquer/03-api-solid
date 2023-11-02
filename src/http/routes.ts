9af7b79cf2f15f3d08da1214e91611f3d50956be
LucasFlaquer <lucas.flaquer@gmail.com>
2023-11-02 10:49:03 -0300

    create profile controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
 import { authenticate } from '@/http/controllers/authenticate'
+import { profile } from '@/http/controllers/profile'
 import { register } from '@/http/controllers/register'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  //* AUTHENTICATED ROUTES
+  app.get('/me', profile)
 }
