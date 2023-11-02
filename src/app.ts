bfc72844d440b0863529e305b48d57e39ef1e1c8
LucasFlaquer <lucas.flaquer@gmail.com>
2023-11-02 10:49:16 -0300

    update env

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import { env } from '@/env'
 import { appRoutes } from '@/http/routes'
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 
 export const app = fastify()
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 app.register(appRoutes)
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
