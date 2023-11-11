d2fe14c06d2a16d86406c4e68e151cc97f8bb823
LucasFlaquer <lucas.flaquer@gmail.com>
2023-11-10 23:01:57 -0300

    refresh token

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,18 @@ import { env } from '@/env'
 import { checkInsRoutes } from '@/http/controllers/check-ins/routes'
 import { gymsRoutes } from '@/http/controllers/gyms/routes'
 import { usersRoutes } from '@/http/controllers/users/routes'
+import fastifyCookie from '@fastify/cookie'
 import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 
 export const app = fastify()
+app.register(fastifyCookie)
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: '10m',
+  },
 })
 app.register(usersRoutes)
 app.register(gymsRoutes)
