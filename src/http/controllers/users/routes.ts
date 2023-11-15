81682cc187e78bc803af0e7e15ba76d1160e6f36
LucasFlaquer <lucas.flaquer@gmail.com>
2023-11-14 23:59:42 -0300

    refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,12 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { authenticate } from '@/http/controllers/users/authenticate'
 import { profile } from '@/http/controllers/users/profile'
 import { register } from '@/http/controllers/users/register'
+import { refresh } from '@/http/controllers/users/refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
   //* AUTHENTICATED ROUTES
   app.get('/me', { onRequest: [verifyJWT] }, profile)
+  app.patch('/token/refresh', refresh)
 }
