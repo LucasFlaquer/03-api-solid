10186646e2f65b4388f3e06b6a1d248761c26f3e
LucasFlaquer <lucas.flaquer@gmail.com>
2023-11-15 11:43:38 -0300

    add role based authentication

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,11 +2,12 @@ import { create } from '@/http/controllers/gyms/create'
 import { nearby } from '@/http/controllers/gyms/nearby'
 import { search } from '@/http/controllers/gyms/search'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 import { FastifyInstance } from 'fastify'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
